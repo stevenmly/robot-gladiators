d5c629dd206236a1d6d7d07bf228009a9719c62e
Steven Ly <ly.steven16@gmail.com>
2020-12-18 15:09:03 -0600

    Blank/null player name bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -174,8 +174,17 @@ var randomNumber = function(min, max) {
   return value;
 }
 
+// function to set up name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name == null) {
+    name = prompt("What is your robot's name?");
+  }
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
